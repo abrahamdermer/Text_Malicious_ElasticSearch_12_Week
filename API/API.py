23b8422a473f7193f01ecc828a19cd412c43b9e4
Avram <ad@dermer.co.il>
2025-09-01 21:16:32 +0300

    by 2

---
+++ API/API.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from manager import Manager
+app = FastAPI()
+
+manager = Manager()
+
+
+@app.get("/tweets")
+def read_root():
+    return manager.dal.get_all_documents()
+
+@app.get("/tweets_tow_weapons")
+def read_root():
+    return manager.dal.get_all_documents()
